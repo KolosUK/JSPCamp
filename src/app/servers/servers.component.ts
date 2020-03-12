cdfbf6a7c7efd2e301d36bee08da349ef8011503
Khrystyna Derenivska <khrisderenivska@gmail.com>
2020-03-12 16:54:56 +0200

    deleted server folder

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,10 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
