6b4a770b434864a50217d6259dd1ec41a5eefaa1
Khrystyna Derenivska <khrisderenivska@gmail.com>
2020-03-17 23:03:23 +0200

    implement edit task

--- src/app/task-item/task-item.component.ts
+++ src/app/task-item/task-item.component.ts
@@ -8,4 +8,16 @@ import Task from '../task';
 })
 export class TaskItemComponent {
   @Input() task: Task;
+  isEditMode: boolean = false;
+  editedTaskName: string;
+
+  onEditClick() {
+    this.isEditMode = true;
+    this.editedTaskName = this.task.name;
+  }
+
+  onSaveClick() {
+    this.task.name = this.editedTaskName;
+    this.isEditMode = false;
+  }
 }
