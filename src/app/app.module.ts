891dd883a26d03cbc530cd3293ba633f899318e7
Khrystyna Derenivska <khrisderenivska@gmail.com>
2020-03-19 22:30:08 +0200

    removed tasks-to-do-list component and comments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,11 @@ import { NgModule, Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { TasksToDoListComponent } from './tasks-to-do-list/tasks-to-do-list.component';
 import { NewItemComponent } from './new-item/new-item.component';
 import { TaskItemComponent } from './task-item/task-item.component';
 
 @NgModule({
-  declarations: [AppComponent, TasksToDoListComponent, NewItemComponent, TaskItemComponent],
+  declarations: [AppComponent, NewItemComponent, TaskItemComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
