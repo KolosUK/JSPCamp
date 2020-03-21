9dc8609a7595ddfa8d639f4113b04071a1202b81
Khrystyna Derenivska <khrisderenivska@gmail.com>
2020-03-21 23:45:52 +0200

    error message if no-name task is created(upd)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,30 @@ import Task from './task';
 export class AppComponent {
   tasks: Task[] = new Array();
   taskName: string;
+  isError: boolean = false;
   errorEmptyField: string = 'You have to name your task';
 
   constructor() {}
 
+  validateInput() {
+    if (!this.taskName) {
+      this.isError = true;
+    } else {
+      this.isError = false;
+    }
+  }
+
   onCreateButtonClick() {
-    this.tasks.push(new Task(this.tasks.length, this.taskName));
+    this.validateInput();
+    if (!this.isError) {
+      this.tasks.push(new Task(this.tasks.length, this.taskName));
+    }
+  }
+
+  onTaskNameChange(taskName: string): void {
+    if (this.taskName) {
+      this.validateInput();
+    }
   }
 
   onDelete(id: number) {
