3e59334d993dc27a208c0e951b0d4599eaa6bbb8
taraswithjazz <43855967+taraswithjazz@users.noreply.github.com>
2020-03-23 21:47:05 +0200

    Code refactoring
    Resolving of PR comments and divide project into two components

--- Chervinka_Taras/todolist/src/app/app.module.ts
+++ Chervinka_Taras/todolist/src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TodoComponent } from './todo/todo.component';
+import { TitleComponent } from './title/title.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoComponent
+    TodoComponent,
+    TitleComponent
   ],
   imports: [
     BrowserModule,
--- Chervinka_Taras/todolist/src/app/todo/todo.component.ts
+++ Chervinka_Taras/todolist/src/app/todo/todo.component.ts
@@ -1,66 +1,54 @@
 import { Component, Input, EventEmitter, Output, OnInit } from '@angular/core';
-
+import { Task } from '../interfaces/task';
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.css']
 })
 export class TodoComponent implements OnInit {
-disabled:boolean;
-tasks:Task[];
-taskName:string;
-index:number;
-idForTask:number;
+  disabled: boolean;
+  tasks: Task[];
+  taskName: string;
+  index: number;
+  idForTask: number;
 
-@Output() edited = new EventEmitter();
-isEdited(){
-  this.edited.emit()
-}
-onEdit(){
-this.taskName
-}
+
   constructor() {
-    // setTimeout(()=>{
-    //   this.disabled = false;
-    // },2000);
-   }
-  
+  }
+
   ngOnInit() {
-    this.idForTask = 2;
+    this.idForTask = 0;
     this.taskName = '';
-    this.tasks = []
+    this.tasks = [];
   }
 
   addTask(): void {
-  if(this.taskName.trim().length===0) {
-    this.disabled = false
-  return;
-  }
+    if (this.taskName.trim().length === 0) {
+      this.disabled = false;
+      return;
+    }
     this.tasks.push({
-      id:this.idForTask,
-      name:this.taskName,
-      completed:false,
-      editing:false
-    })
+      id: this.idForTask,
+      name: this.taskName,
+      completed: false,
+      editing: false
+    });
     this.idForTask++;
     this.taskName = '';
   }
-  editTask(task:Task){
-    task.editing=true;
+  editTask(task: Task) {
+    task.editing = true;
+    return;
   }
 
-  doneEdit(task:Task){
+  doneEdit(task: Task) {
     task.editing = false;
+    return;
   }
 
-  deleteTask(id:number):void{
-  this.tasks = this.tasks.filter(task=>task.id !== id);
+  deleteTask(id: number): void {
+    this.tasks = this.tasks.filter(task => task.id !== id);
+    return;
   }
 
-}
-interface Task {
-  id:number,
-  name:string,
-  completed:boolean,
-  editing:boolean
 }
