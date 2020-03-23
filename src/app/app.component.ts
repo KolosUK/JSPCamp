d88fc2d7de075bfde8b8d83e24733b901183be5b
Khrystyna Derenivska <khrisderenivska@gmail.com>
2020-03-23 12:17:58 +0200

    extract code into task-to-add component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,40 +1,8 @@
 import { Component } from '@angular/core';
-import Task from './task';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  tasks: Task[] = new Array();
-  taskName: string;
-  isError = false;
-  errorEmptyField = 'You have to name your task';
-
-  validateInput() {
-    if (!this.taskName) {
-      this.isError = true;
-    } else {
-      this.isError = false;
-    }
-  }
-
-  onCreateButtonClick() {
-    this.validateInput();
-    if (!this.isError) {
-      this.tasks.push(new Task(this.tasks.length, this.taskName));
-      this.taskName = '';
-    }
-  }
-
-  onTaskNameChange(taskName: string): void {
-    if (this.taskName) {
-      this.validateInput();
-    }
-  }
-
-  onDelete(id: number) {
-    this.tasks = this.tasks.filter(el => el.id !== id);
-  }
-}
+export class AppComponent {}
--- src/app/task-to-add/task-to-add.component.ts
+++ src/app/task-to-add/task-to-add.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Task from '../task';
 
 @Component({
   selector: 'app-task-to-add',
@@ -6,6 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task-to-add.component.css']
 })
 export class TaskToAddComponent implements OnInit {
+  tasks: Task[] = new Array();
+  taskName: string;
+  isError = false;
+  errorEmptyField = 'You have to name your task';
+
+  validateInput() {
+    if (!this.taskName) {
+      this.isError = true;
+    } else {
+      this.isError = false;
+    }
+  }
+
+  onTaskNameChange(taskName: string): void {
+    if (this.taskName) {
+      this.validateInput();
+    }
+  }
+
+  onCreateButtonClick() {
+    this.validateInput();
+    if (!this.isError) {
+      this.tasks.push(new Task(this.tasks.length, this.taskName));
+      this.taskName = '';
+    }
+  }
+
+  onDelete(id: number) {
+    this.tasks = this.tasks.filter(el => el.id !== id);
+  }
+
   constructor() {}
 
   ngOnInit(): void {}
