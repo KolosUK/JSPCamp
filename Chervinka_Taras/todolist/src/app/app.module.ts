efb69b416d3ff2661189e2048d9de9a8412b71b4
taraswithjazz <43855967+taraswithjazz@users.noreply.github.com>
2020-03-24 14:02:28 +0200

    Two Components fix
    Legit two component divide

--- Chervinka_Taras/todolist/src/app/app.module.ts
+++ Chervinka_Taras/todolist/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { TodoComponent } from './todo/todo.component';
 import { TitleComponent } from './title/title.component';
+import { TodoTableComponent } from './todo-table/todo-table.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoComponent,
-    TitleComponent
+    TitleComponent,
+    TodoTableComponent
   ],
   imports: [
     BrowserModule,
--- Chervinka_Taras/todolist/src/app/todo/todo.component.ts
+++ Chervinka_Taras/todolist/src/app/todo/todo.component.ts
@@ -1,50 +1,18 @@
 import { Component, Input, EventEmitter, Output, OnInit } from '@angular/core';
-import { Task } from '../interfaces/task';
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.css']
 })
 export class TodoComponent implements OnInit {
-  disabled: boolean;
-  tasks: Task[];
-  taskName: string;
-  index: number;
-  idForTask: number;
+  @Input() public inputValue: string;
+  @Output() inputEvent = new EventEmitter<string>();
+  onTaskAdd(value: string) {
+    this.inputEvent.emit(value);
+  }
 
   ngOnInit() {
-    this.idForTask = 0;
-    this.taskName = '';
-    this.tasks = [];
-  }
-
-  addTask(): void {
-    if (this.taskName.trim().length === 0) {
-      this.disabled = false;
-      return;
-    }
-    this.tasks.push({
-      id: this.idForTask,
-      name: this.taskName,
-      completed: false,
-      editing: false
-    });
-    this.idForTask++;
-    this.taskName = '';
-  }
-  editTask(task: Task) {
-    task.editing = true;
-    return;
-  }
-
-  doneEdit(task: Task) {
-    task.editing = false;
-    return;
-  }
-
-  deleteTask(id: number): void {
-    this.tasks = this.tasks.filter(task => task.id !== id);
-    return;
+    this.inputValue = '';
   }
 
 }
